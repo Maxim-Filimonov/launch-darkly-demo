14819f22a5fba8c5a31d183f1809708f6f6b6d09
Maxim Filimonov <tpaktopsp@gmail.com>
2022-01-21 12:55:33 +0800

    add support for env vairable override

--- index.ts
+++ index.ts
@@ -1,29 +1,30 @@
-import * as LaunchDarkly from 'launchdarkly-node-server-sdk';
+import * as LaunchDarkly from "launchdarkly-node-server-sdk";
 
 // TODO : Enter your LaunchDarkly SDK key here
-const client = LaunchDarkly.init("YOUR_SDK_KEY");
+const client = LaunchDarkly.init(process.env.LD_API_KEY || "");
 
 const user = {
-   "firstName":"Bob",
-   "lastName":"Loblaw",
-   "key":"louise@example.com",
-   "custom":{
-      "groups":"beta_testers"
-   }
+  firstName: "Bob",
+  lastName: "Loblaw",
+  email: "louise@example.com",
+  key: process.env.KEY || "default",
+  custom: {
+    groups: "beta_testers",
+  },
 };
 
-client.once('ready', function() {
+client.once("ready", function () {
   // TODO : Enter the key for your feature flag here
-  client.variation("enable-pinning", user, false, function(err, showFeature) {
+  client.variation("enable-pinning", user, false, function (err, showFeature) {
     client.track("event-called", user);
     if (showFeature) {
       // application code to show the feature
-      console.log("Showing your feature to " + user.key );
+      console.log("Showing your feature to " + user.email);
     } else {
-      // the code to run if the feature is off 
-      console.log("Not showing your feature to " + user.key);
+      // the code to run if the feature is off
+      console.log("Not showing your feature to " + user.email);
     }
-    client.flush(function() {
+    client.flush(function () {
       client.close();
     });
   });
